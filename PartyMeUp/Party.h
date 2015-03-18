8d9efb2f317a2f8ad2f328178c33b9be734b76df
Anderson Gralha <anderson.gralha@gmail.com>
2015-03-18 08:46:49 -0300

    Changed folder structure

---
+++ PartyMeUp/Party.h
@@ -0,0 +1,13 @@
+//
+//  Party.h
+//  PartyMeUp
+//
+//  Created by Anderson Rogério da Silva Gralha on 3/18/15.
+//  Copyright (c) 2015 Anderson Gralha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Party : NSObject
+
+@end
---
+++ PartyMeUp/Party.m
@@ -0,0 +1,13 @@
+//
+//  Party.m
+//  PartyMeUp
+//
+//  Created by Anderson Rogério da Silva Gralha on 3/18/15.
+//  Copyright (c) 2015 Anderson Gralha. All rights reserved.
+//
+
+#import "Party.h"
+
+@implementation Party
+
+@end
